6015d0da30a02900298a9e8983de0e3778d9c880
afh1760 <61475425+afh1760@users.noreply.github.com>
2020-04-19 06:08:19 -0500

    Added Asset Accounts

--- src/BooKeeper.java
+++ src/BooKeeper.java
@@ -2,7 +2,7 @@
 //
 // Title:           BooKeeper.java 
 // Files:           (a list of all source files used by that program)
-// Course:          CS400 Lec001, Spring, 2020
+// Course:          CS400, Spring 2020
 //
 // Author:          Qingqi Wu, Alexander Hertadi, Benjamin Tarmann
 // Email:           qwu86@wisc.edu
@@ -35,8 +35,48 @@ public class BooKeeper {
    * @param args
    */
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    int[] currentAsset = {1,0,0};
+    int[] nonCurrentAsset = {1,0,1};
+    
+    Account cash = new Account("Cash");
+    cash.addIdentifier(currentAsset);
+    
+    Account sInvest = new Account("Short-term Investments");
+    sInvest.addIdentifier(currentAsset);
+    
+    Account accRec = new Account("Account Receivable");
+    accRec.addIdentifier(currentAsset);
+    
+    Account prepaidExp = new Account("Prepaid Expenses");
+    prepaidExp.addIdentifier(currentAsset);
+    
+    Account inventory = new Account("Inventory");
+    inventory.addIdentifier(currentAsset);
+    
+    Account supplies = new Account("Supplies");
+    supplies.addIdentifier(currentAsset);
+    
+    Account lInvest = new Account("Long-term Investments");
+    lInvest.addIdentifier(nonCurrentAsset);
 
+    Account land = new Account("Land");
+    land.addIdentifier(nonCurrentAsset);
+
+    Account buildings = new Account("Buildings");
+    buildings.addIdentifier(nonCurrentAsset);
+    
+    Account equipment = new Account("Equipment");
+    equipment.addIdentifier(nonCurrentAsset);
+    
+    Account vehicles = new Account("Vehicles");
+    vehicles.addIdentifier(nonCurrentAsset);
+    
+    Account furniture = new Account("Furniture & Fixtures");
+    furniture.addIdentifier(nonCurrentAsset);
+    
+    Account depres = new Account("Accumulated Depreciation");
+    depres.addIdentifier(nonCurrentAsset);
+    
   }
 
 }
