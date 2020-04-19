22eb26c0b046c28c45c1daf98486fb0f96b4fa9a
Qingqi Wu <qwu86@wisc.edu>
2020-04-18 19:30:39 -0500

    Added accounts and started the main class

---
+++ src/Account.java
@@ -0,0 +1,34 @@
+//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
+//
+// Title:           Account.java 
+// Files:           (a list of all source files used by that program)
+// Course:          CS400 Lec001, Spring, 2020
+//
+// Author:          Qingqi Wu
+// Email:           qwu86@wisc.edu
+// Lecturer's Name: Debra Deppeler
+//
+///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
+//
+// Students who get help from sources other than their partner must fully 
+// acknowledge and credit those sources of help here.  Instructors and TAs do 
+// not need to be credited here, but tutors, friends, relatives, room mates, 
+// strangers, and others do.  If you received no outside help from either type
+//  of source, then please explicitly indicate NONE.
+//
+// Persons:         NONE
+// Online Sources:  NONE
+//
+/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
+
+/**
+ * Account - TODO Description
+ * 
+ * Bugs: none known
+ * @author Qingqi Wu
+ * @version 1.0
+ * 
+ */
+public class Account {
+
+}
---
+++ src/BooKeeper.java
@@ -0,0 +1,42 @@
+//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
+//
+// Title:           BooKeeper.java 
+// Files:           (a list of all source files used by that program)
+// Course:          CS400 Lec001, Spring, 2020
+//
+// Author:          Qingqi Wu
+// Email:           qwu86@wisc.edu
+// Lecturer's Name: Debra Deppeler
+//
+///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
+//
+// Students who get help from sources other than their partner must fully 
+// acknowledge and credit those sources of help here.  Instructors and TAs do 
+// not need to be credited here, but tutors, friends, relatives, room mates, 
+// strangers, and others do.  If you received no outside help from either type
+//  of source, then please explicitly indicate NONE.
+//
+// Persons:         NONE
+// Online Sources:  NONE
+//
+/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
+
+/**
+ * BooKeeper - TODO Description
+ * 
+ * Bugs: none known
+ * @author Qingqi Wu
+ * @version 1.0
+ * 
+ */
+public class BooKeeper {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
